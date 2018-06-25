5a22cd9468f20bb4ba924b16f45c883fcc5d8976
Tony Robertson <tonyr729@gmail.com>
2018-06-25 15:55:47 -0600

    Add body parser dependency

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
 
-app.get('/', (request, response) => {
-  response.send('Server Working');
-});
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+// app.get('/', (request, response) => {
+//   response.send('Server Working');
+// });
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
