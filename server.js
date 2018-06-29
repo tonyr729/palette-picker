c0b9703935be04ab98fd269eece65737043b25af
Tony Robertson <tonyr729@gmail.com>
2018-06-29 11:23:47 -0600

    Add comments for server file

--- server.js
+++ server.js
@@ -1,34 +1,47 @@
-const express = require('express');
 // Import express
-const app = express();
+const express = require('express');
 // Instantiate express as app
+const app = express();
+// Import bodyParser to be able to parse body from the request object
 const bodyParser = require('body-parser');
-
+// Set enviorment port to designated or 3000
 app.set('port', process.env.PORT || 3000);
-
+// Tell app to use bodyParser
 app.use(bodyParser.json());
+// Tell app to use public file for static frontend
 app.use(express.static('public'));
-
+// Define dev enviorment for knex
 const environment = process.env.NODE_ENV || 'development';
+// Import knex using knexfile with defined enviorment
 const configuration = require('./knexfile')[environment];
+// Import database from knex
 const database = require('knex')(configuration);
 
+// Tell express for the get method of the given endpoint do the following
 app.get('/api/v1/projects', (request, response) => {
+  // Access database through knex and select the 'projects' table
   database('projects').select()
+  // Take the returned promise and after consuming it with then 
     .then((projects) => {
+      // Return with a succesfull status code and a json parsed version of the project
       response.status(200).json(projects);
     })
+    // If there is an error
     .catch((error) => {
+      // Return a json'd error
       response.status(500).json({ error });
     });
 });
 
+// Same thing as the previous function but now accessing palettes
 app.get('/api/v1/projects/:id/palettes', (request, response) => {
+  // Use .where() to filter the returned table elements that only have the matching id that comes from the params.
   database('palettes').where('project_id', request.params.id).select()
     .then(palettes => {
       if (palettes.length) {
         response.status(200).json(palettes);
       } else {
+        // If you cant find a matching return a message
         response.status(404).json({ 
           error: `Could not find palette with project id of ${request.params.id}`
         });
@@ -39,19 +52,22 @@ app.get('/api/v1/projects/:id/palettes', (request, response) => {
     });
 });
 
+// Tell express for the post method of the given endpoint do the following
 app.post('/api/v1/projects', (request, response) => {
+  // Use bodyParser to parse the project object from the request body
   const project = request.body;
-
+  // param validation
   for (let requiredParameter of ['name']) {
     if (!project[requiredParameter]) {
       return response
-        .status(422)
-        .send({ error: `Expected format: { name: <String> }. You're missing a "${requiredParameter}" property.` });
+      .status(422)
+      .send({ error: `Expected format: { name: <String> }. You're missing a "${requiredParameter}" property.` });
     }
   }
-
+  // Take the project object and insert it into the database 
   database('projects').insert(project, 'id')
     .then(project => {
+      // return the generated id
       response.status(201).json({ id: project[0] })
     })
     .catch(error => {
@@ -59,6 +75,7 @@ app.post('/api/v1/projects', (request, response) => {
     });
 });
 
+// Same method as above but for palettes endpoint
 app.post('/api/v1/projects/:id/palettes', (request, response) => {
   const palette = request.body;
 
@@ -79,8 +96,12 @@ app.post('/api/v1/projects/:id/palettes', (request, response) => {
     });
 });
 
+// Tell express for the delete method of the given endpoint do the following
 app.delete('/api/v1/projects/:id/palettes/:id2', (request, response) => {
+  // Use bodyParser to parse the id of the palette object from the request body
   const { id2 } = request.params;
+
+  // Use .where() to find the matching palette and .del() it from the database
   database('palettes').where({id: id2}).del()
     .then(palette => {
       response.sendStatus(204)
@@ -90,6 +111,7 @@ app.delete('/api/v1/projects/:id/palettes/:id2', (request, response) => {
     });
 });
 
+// Tell app to listen to the given port to run the server
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
