8c8f220f0288da1edd70c19d550af42d2deed625
Tony Robertson <tonyr729@gmail.com>
2018-07-05 10:01:00 -0600

    Change test server name

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   test: {
     client: 'pg',
-    connection: 'postgres://localhost/mocha_chai_tv_shows_test',
+    connection: 'postgres://localhost/palette_picker_test',
     migrations: {
       directory: __dirname + '/db/migrations'
     },
